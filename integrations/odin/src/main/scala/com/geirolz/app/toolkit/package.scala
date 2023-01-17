726797ec0487f93584975dbdcdd81bdfff86280f
David Geirola <david.geirola@gmail.com>
2023-01-17 20:08:09 +0100

    Fix CICD

--- integrations/odin/src/main/scala/com/geirolz/app/toolkit/package.scala
+++ integrations/odin/src/main/scala/com/geirolz/app/toolkit/package.scala
@@ -5,14 +5,14 @@ import io.odin.Logger
 
 package object toolkit {
 
-  implicit def odinLoggerAdapter[ODIN_LOGGER[_[_]] <: Logger[_[_]]]: LoggerAdapter[ODIN_LOGGER] =
+  implicit def odinLoggerAdapter[ODIN_LOGGER[F[_]] <: Logger[F[_]]]: LoggerAdapter[ODIN_LOGGER] =
     new LoggerAdapter[ODIN_LOGGER] {
-      override def toToolkit[F[_]](appLogger: ODIN_LOGGER[F]): ToolkitLogger[F] =
+      override def toToolkit[F[_]](odinLogger: ODIN_LOGGER[F]): ToolkitLogger[F] =
         new ToolkitLogger[F] {
           override def info(message: => String): F[Unit] =
-            appLogger.info(message)
+            odinLogger.info(message)
           override def error(ex: Throwable)(message: => String): F[Unit] =
-            appLogger.error(message, ex)
+            odinLogger.error(message, ex)
         }
     }
 }
