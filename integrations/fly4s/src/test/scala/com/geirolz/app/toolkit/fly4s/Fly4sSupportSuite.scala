a27f89ea78c15214d1d5414f0cd8adecf5451af0
geirolz <david.geirola@gmail.com>
2023-06-30 18:10:59 +0200

    Fix test

--- integrations/fly4s/src/test/scala/com/geirolz/app/toolkit/fly4s/Fly4sSupportSuite.scala
+++ integrations/fly4s/src/test/scala/com/geirolz/app/toolkit/fly4s/Fly4sSupportSuite.scala
@@ -8,31 +8,28 @@ import com.geirolz.app.toolkit.{App, SimpleAppInfo}
 class Fly4sSupportSuite extends munit.CatsEffectSuite {
 
   test("Syntax works as expected") {
-    assertIO_(
-      App[IO]
-        .withInfo(
-          SimpleAppInfo.string(
-            name         = "toolkit",
-            version      = "0.0.1",
-            scalaVersion = "2.13.10",
-            sbtVersion   = "1.8.0"
-          )
+    App[IO]
+      .withInfo(
+        SimpleAppInfo.string(
+          name         = "toolkit",
+          version      = "0.0.1",
+          scalaVersion = "2.13.10",
+          sbtVersion   = "1.8.0"
         )
-        .withConfig(
-          TestConfig(
-            dbUrl      = "jdbc:postgresql://localhost:5432/toolkit",
-            dbUser     = Some("postgres"),
-            dbPassword = Some("postgres".toCharArray)
-          )
+      )
+      .withConfig(
+        TestConfig(
+          dbUrl      = "jdbc:postgresql://localhost:5432/toolkit",
+          dbUser     = Some("postgres"),
+          dbPassword = Some("postgres".toCharArray)
         )
-        .withoutDependencies
-        .provideOne(_ => IO.unit)
-        .beforeProvidingMigrateDatabaseWithConfig(
-          url      = _.dbUrl,
-          user     = _.dbUser,
-          password = _.dbPassword
-        )
-        .run_
-    )
+      )
+      .withoutDependencies
+      .provideOne(_ => IO.unit)
+      .beforeProvidingMigrateDatabaseWithConfig(
+        url      = _.dbUrl,
+        user     = _.dbUser,
+        password = _.dbPassword
+      )
   }
 }
