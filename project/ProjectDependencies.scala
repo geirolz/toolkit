41c2a5a1808488f54d58b2365279e5028a40ad41
geirolz <david.geirola@gmail.com>
2023-07-09 14:20:24 +0200

    Drop coverage for scala 3

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 import scala.language.postfixOps
 
@@ -6,7 +6,6 @@ object ProjectDependencies {
 
   private val catsVersion              = "2.9.0"
   private val catsEffectVersion        = "3.5.0"
-  private val refinedVersion           = "0.10.1"
   private val circeVersion             = "0.14.5"
   private val circeGenericExtraVersion = "0.14.3"
   private val pureConfigVersion        = "0.17.4"
