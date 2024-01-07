6fd0c490f808eae30bb693cbde7a3638f03b2cba
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-01-07 00:13:33 +0000

    Update http4s-dsl, http4s-ember-server to 0.23.25

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -15,7 +15,7 @@ object ProjectDependencies {
   private val slf4Version              = "2.0.10"
   private val log4catsVersion          = "2.6.0"
   private val odinVersion              = "0.13.0"
-  private val http4sVersion            = "0.23.24"
+  private val http4sVersion            = "0.23.25"
   private val fs2Version               = "3.9.3"
   private val scalacheck               = "1.17.0"
 
