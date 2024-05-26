0b1bfb77beb63581a6412ee1ef15ad36c31119eb
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-05-26 00:21:09 +0000

    Update munit, munit-scalacheck to 1.0.0

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -10,7 +10,7 @@ object ProjectDependencies {
   private val circeGenericExtraVersion = "0.14.3"
   private val pureConfigVersion        = "0.17.6"
   private val fly4sVersion             = "1.0.1"
-  private val munitVersion             = "0.7.29"
+  private val munitVersion             = "1.0.0"
   private val munitEffectVersion       = "1.0.7"
   private val slf4Version              = "2.0.12"
   private val log4catsVersion          = "2.6.0"
