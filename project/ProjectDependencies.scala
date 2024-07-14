a17e025db0d40f18aa961d5971490445c482a512
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-07-14 00:23:53 +0000

    Update fly4s to 1.0.6

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -9,7 +9,7 @@ object ProjectDependencies {
   private val circeVersion             = "0.14.9"
   private val circeGenericExtraVersion = "0.14.3"
   private val pureConfigVersion        = "0.17.6"
-  private val fly4sVersion             = "1.0.5"
+  private val fly4sVersion             = "1.0.6"
   private val munitVersion             = "1.0.0"
   private val munitEffectVersion       = "2.0.0"
   private val slf4Version              = "2.0.13"
